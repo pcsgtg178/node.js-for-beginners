c73a121d461900dcc24f8e9131aec0972f3536f1
pcsgtg178 <pcs.gtg@gmail.com>
2020-03-12 17:07:50 +0700

    update Part12 second time

--- Part12/app.js
+++ Part12/app.js
@@ -8,7 +8,10 @@ app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'static','index.html'));
 })
 app.get('/:userQuery',(req,res)=>{
-    res.render('index',{data : {userQuery: req.params.userQuery}});
+    res.render('index',{data : {userQuery: req.params.userQuery,
+                                searchResults: ['book1','book2','book3'],
+                                loggedIn: true,
+                                username: 'playboys'}});
 });
 
 app.listen('3000',()=>{console.log('server is running 3000')});
